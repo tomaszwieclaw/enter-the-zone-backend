82c07b47a58ae1ecc3a5f73f0a0e3db782572392
anatoliy.ornatovskyy <anatoliy.ornatovskyy@bullhorn.com>
2023-10-01 21:20:33 +0200

    Adding CORS for POST

---
+++ src/main/java/com/teaminternational/enterthezone/domain/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.teaminternational.enterthezone.domain.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+public class WebSecurityConfiguration {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
